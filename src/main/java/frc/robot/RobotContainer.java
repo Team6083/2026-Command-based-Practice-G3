a99e7f73c22f73d9aea08172b32e8d2ca118f7ba
tsen991007 <tsen.991007@gmail.com>
2025-12-28 14:32:49 +0800

    add: joystick and motors

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,10 +4,21 @@
 
 package frc.robot;
 
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import frc.robot.Constants.ControllerConstants;
+import frc.robot.Constants.MotorConstants;
 
 public class RobotContainer {
+  XboxController joy = new XboxController(ControllerConstants.port);
+  VictorSPX vic1 = new VictorSPX(MotorConstants.vic1ID);
+  VictorSPX vic2 = new VictorSPX(MotorConstants.vic2ID);
+  TalonSRX tal1 = new TalonSRX(MotorConstants.tal1ID);
+  TalonSRX tal2 = new TalonSRX(MotorConstants.tal2ID);
   public RobotContainer() {
     configureBindings();
   }
