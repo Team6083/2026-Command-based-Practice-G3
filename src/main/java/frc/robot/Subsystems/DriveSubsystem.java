46549f6c165030b29ada966efb5f71854d39f9f7
tsen991007 <tsen.991007@gmail.com>
2025-12-29 20:14:23 +0800

    add: joystick

--- src/main/java/frc/robot/Subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/Subsystems/DriveSubsystem.java
@@ -2,15 +2,17 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.subsystems;
+package frc.robot.Subsystems;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class DriveSubsystem extends SubsystemBase {
+  XboxController joy = new XboxController(Constants.ControllerConstants.port);
   VictorSPX vic1 = new VictorSPX(Constants.MotorConstants.vic1ID);
   VictorSPX vic2 = new VictorSPX(Constants.MotorConstants.vic2ID);
   TalonSRX tal1 = new TalonSRX(Constants.MotorConstants.tal1ID);
