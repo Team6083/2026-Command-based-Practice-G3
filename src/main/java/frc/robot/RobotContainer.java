3d2d411260b9b9c2f69352a4cc0b026d3ef10738
tsen991007 <tsen.991007@gmail.com>
2026-01-04 12:31:23 +0800

    DriveSubsystem package frc.robot.subsystems

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -8,6 +8,7 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.commands.DriveCmd;
+import frc.robot.subsystems.DriveSubsystem;
 
 public class RobotContainer {
   CommandXboxController controller = new CommandXboxController(Constants.ControllerConstants.port);
--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -2,12 +2,13 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot;
+package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 
 public class DriveSubsystem extends SubsystemBase {
   VictorSPX vic1 = new VictorSPX(Constants.MotorConstants.vic1ID);
